d893fc971912bfde35593cc5c46f88dfb0670178
Oliver Moreno <156409897+sysad-ormoreno@users.noreply.github.com>
2026-04-02 18:32:34 +0800

    Update database.py

---
+++ backend/app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_all, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# This creates a local SQLite database file named 'usher_app.db'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./usher_app.db"
+
+# The engine is the actual connection to the database
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Each instance of the SessionLocal class will be a database session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base is the class we will inherit from to create our Models
+Base = declarative_base()
